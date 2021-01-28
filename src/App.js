e67acdfa9b8f057e50370eb2db483a81f188bf4c
aditya <eng_aditya_acharya@hotmmail.com>
2021-01-29 00:50:57 +0530

    changed the title

--- src/App.js
+++ src/App.js
@@ -5,17 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Thougths for Us</h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.pinterest.com/iialvarezii/the-thought-for-the-day/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react
+          Click here for thoughts take picture examples
         </a>
       </header>
     </div>
